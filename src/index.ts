918eff4fa066c6e1fdd1feb2aed202a4c1515061
Shahab Movahhedi <45717968+movahhedi@users.noreply.github.com>
2025-02-08 11:28:36 +0330

    feat: add force empty flag

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ program
 	.argument("<dir-name>", "Directory of the project")
 	.requiredOption("-t, --template <template>", "Template name")
 	.option("-f, --force", "Force create a project in a non-empty directory")
+	.option("-e, --force-empty", "Force create a project in a non-empty directory, emptying it first.")
 	.option("-G, --no-git", "Don't initialize a git repository")
 	.option("-i, --install", "Install dependencies after creating the project")
 	.action(async (dirNameRaw) => {
@@ -27,15 +28,26 @@ program
 		const template = options.template;
 		const dirName = Path.resolve(dirNameRaw);
 		const templatePath = Path.resolve(__dirname, `../templates/${template}`);
+		const isForce = options.force;
+		const isForceEmpty = options.forceEmpty;
 
 		try {
-			const doesDirExist = await fs.access(dirName);
+			// const doesDirExist = await fs.access(dirName);
 
 			const isDirFull = (await fs.readdir(dirName)).length > 0;
 
-			if (isDirFull) {
+			if (isDirFull && isForceEmpty) {
+				// delete the directory, and recreate it
+
+				console.log("Emptying the directory...");
+				await fs.rm(dirName, {
+					recursive: true,
+				});
+			}
+
+			if (isDirFull && !(isForce || isForceEmpty)) {
 				console.error(
-					"The directory is not empty. To force create a project in a non-empty directory, use the `--force` flag."
+					"The directory is not empty. To force create a project in a non-empty directory, use the `--force` flag.\n To empty the directory first, use the `--force-empty` flag."
 				);
 				return;
 			}
@@ -101,7 +113,9 @@ program
 		console.log("Project created successfully at the directory:", dirName, "\n");
 		console.log("To start the project, run the following commands:\n");
 		console.log(`\tcd ${dirNameRaw}`);
-		console.log("\tyarn install");
+		if (!options.install) {
+			console.log("\tyarn install");
+		}
 
 		console.log("\nHappy coding!");
 	});
